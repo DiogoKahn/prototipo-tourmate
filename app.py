097976c2b643971d01d19b2cb7964b368dd0cd31
DiogoKahm <rm92928@fiap.com.br>
2023-05-03 09:57:25 -0300

    att

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def recomendacao(prefs):
                     "content": "You are travel guide assistant that responds all travel recomendation in a python dictonary with each key being a place or city and its contents being a list of strings of recomendations of things to do in that place or city. Respond with code only."})
     
     messages.append({"role": "user", "content": 
-                    f"Give me travel recomendations in {prefs['Estado']}'s state in {prefs['Clima']} with {prefs['tipo_viagem']} porposes."})
+                    f"Give me travel recomendations in {prefs['Estado']} state in {prefs['Clima']} with {prefs['tipo_viagem']} porposes."})
     data = {"model": "gpt-3.5-turbo", "messages": messages}
     response = requests.post(url, headers=headers, json=data)
     reply = response.json()["choices"][0]["message"]["content"]
@@ -66,6 +66,26 @@ def recomendacao(prefs):
     resp = resp.replace("\n", "").replace("\t", "").replace("  ", "")
 
     resp = eval(resp)
+
+    # resp = {'São Paulo': 
+    #         ['Visit the São Paulo Museum of Art (MASP) and see works by famous artists like Van Gogh, Picasso and Monet.',
+    #          'Explore the trendy Vila Madalena neighborhood, known for its street art, restaurants and nightlife.',
+    #          "Take a walk around Ibirapuera Park, which is São Paulo's largest park and has several attractions including museums and a Japanese garden.",
+    #          "Visit the Municipal Market of São Paulo, one of the city's main attractions where you can find several typical foods from Brazil and the world."
+    #         ],
+    #         'Campos do Jordão': 
+    #          ['Enjoy the cold weather in the charming winter wonderland of Campos do Jordão.',
+    #           'Visit the Frei Baraúna Convent, a historic building with beautiful architecture and stunning views of the surrounding mountains.',
+    #           'Take a ride on the cable car to the top of the Elephant Hill for breathtaking views of the region.',
+    #           'Explore the Campos do Jordão State Park, which has over 8,000 hectares of stunning nature and wildlife to discover.'
+    #         ],
+    #         'Ilhabela': 
+    #          ["Relax on the island's beautiful beaches and swim in the turquoise waters.",
+    #           'Hike to the top of the Baepi Hill for panoramic views of the island and surrounding seascape.',
+    #           'Visit the Toca Waterfall, a stunning natural attraction with beautiful pools and surrounding rainforest.',
+    #           'Take a boat tour around the island to explore its many coves, beaches and crystal-clear waters.'
+    #          ]
+    #         }
 
     return resp
 
